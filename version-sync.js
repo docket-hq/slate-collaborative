84bd8b6bc9337a37af6671ee7ed0b71457ed5c12
John Barlow <john@dockethq.com>
2021-01-15 11:57:33 -0500

    fix: updated version-sync

--- version-sync.js
+++ version-sync.js
@@ -1,9 +1,16 @@
 //the purpose of this file is to write a change to the version-sync file in each
 //package, so that when we build all three projects move verisons together.
 const fs = require('fs')
+const { execSync } = require('child_process')
 const data = Date.now()
 
-fs.writeFile('packages/backend/version-sync', data, null, () => {})
-fs.writeFile('packages/client/version-sync', data, null, () => {})
-fs.writeFile('packages/bridge/version-sync', data, null, () => {})
-fs.writeFile('packages/example/version-sync', data, null, () => {})
+fs.writeFileSync('packages/backend/version-sync', data)
+fs.writeFileSync('packages/client/version-sync', data)
+fs.writeFileSync('packages/bridge/version-sync', data)
+fs.writeFileSync('packages/example/version-sync', data)
+
+execSync('git add packages/backend/version-sync')
+execSync('git add packages/client/version-sync')
+execSync('git add packages/bridge/version-sync')
+execSync('git add packages/example/version-sync')
+execSync('git commit -m "docs: version-sync"')
