fa9820fb08c9e900b5e05ad0b6cbed3cd658ab19
John Barlow <john@dockethq.com>
2021-03-01 12:58:09 -0500

    fix: #18: integrate pr60 from upstream

--- packages/bridge/src/convert/remove.ts
+++ packages/bridge/src/convert/remove.ts
@@ -3,65 +3,102 @@ import { Element } from 'slate'
 
 import { toSlatePath, toJS } from '../utils'
 import { getTarget } from '../path'
+import { setDataOp } from './set'
 
 const removeTextOp = (op: Automerge.Diff) => (map: any, doc: Element) => {
-  const { index, path, obj } = op
-
-  const slatePath = toSlatePath(path).slice(0, path?.length)
-
-  let node
-
   try {
-    node = getTarget(doc, slatePath) || map[obj]
+    const { index, path, obj } = op
+
+    const slatePath = toSlatePath(path).slice(0, path?.length)
+
+    let node = map[obj]
+
+    try {
+      node = getTarget(doc, slatePath)
+    } catch (e) {
+      console.error(e, slatePath, op, map, toJS(doc))
+    }
+
+    if (typeof index !== 'number') return
+
+    const text = node?.text?.[index] || '*'
+
+    if (node) {
+      node.text = node?.text
+        ? node.text.slice(0, index) + node.text.slice(index + 1)
+        : ''
+    }
+
+    return {
+      type: 'remove_text',
+      path: slatePath,
+      offset: index,
+      text,
+      marks: []
+    }
   } catch (e) {
-    console.error(e, op, doc)
-  }
-
-  if (typeof index !== 'number') return
-
-  const text = node?.text[index] || '*'
-
-  if (node) {
-    node.text = node.text?.slice(0, index) + node.text?.slice(index + 1)
-  }
-
-  return {
-    type: 'remove_text',
-    path: slatePath,
-    offset: index,
-    text,
-    marks: []
+    console.error(e, op, map, toJS(doc))
   }
 }
 
-const removeNodeOp = ({ index, obj, path }: Automerge.Diff) => (
-  map: any,
-  doc: Element
+const removeNodeOp = (op: Automerge.Diff) => (map: any, doc: Element) => {
+  try {
+    const { index, obj, path } = op
+
+    const slatePath = toSlatePath(path)
+
+    const parent = getTarget(doc, slatePath)
+    const target = parent?.children?.[index as number] ||
+      parent?.[index as number] || { children: [] }
+
+    if (!target) {
+      throw new TypeError('Target is not found!')
+    }
+
+    if (!Number.isInteger(index)) {
+      throw new TypeError('Index is not a number')
+    }
+
+    if (parent?.children?.[index as number]) {
+      parent.children.splice(index, 1)
+      map[obj] = parent?.children
+    } else if (parent?.[index as number]) {
+      parent.splice(index, 1)
+      map[obj] = parent
+    }
+
+    return {
+      type: 'remove_node',
+      path: slatePath.length ? slatePath.concat(index) : [index],
+      node: target
+    }
+  } catch (e) {
+    console.error(e, op, map, toJS(doc))
+  }
+}
+
+const opRemove = (
+  op: Automerge.Diff,
+  [map, ops]: any,
+  doc: any,
+  tmpDoc: Element
 ) => {
-  const slatePath = toSlatePath(path)
-
-  const parent = getTarget(doc, slatePath)
-  const target = parent?.children[index as number] || { children: [] }
-
-  if (!map.hasOwnProperty(obj)) {
-    map[obj] = target
-  }
-
-  return {
-    type: 'remove_node',
-    path: slatePath.length ? slatePath.concat(index) : [index],
-    node: target
-  }
-}
-
-const opRemove = (op: Automerge.Diff, [map, ops]: any) => {
   try {
     const { index, path, obj, type } = op
+
+    if (type === 'map' && path) {
+      // remove a key from map, mapping to slate set a key's value to undefined.
+      if (path[0] === 'children') {
+        ops.push(setDataOp(op, doc)(map, tmpDoc))
+      }
+      return [map, ops]
+    }
 
     if (
       map.hasOwnProperty(obj) &&
       typeof map[obj] !== 'string' &&
-      type !== 'text'
+      type !== 'text' &&
+      map?.obj?.length
     ) {
       map[obj].splice(index, 1)
 
@@ -72,11 +109,9 @@ const opRemove = (op: Automerge.Diff, [map, ops]: any) => {
 
     const key = path[path.length - 1]
 
-    if (key === 'cursors') return [map, ops]
-
     const fn = key === 'text' ? removeTextOp : removeNodeOp
 
-    return [map, [...ops, fn(op)]]
+    return [map, [...ops, fn(op)(map, tmpDoc)]]
   } catch (e) {
     console.error(e, op, toJS(map))
 
